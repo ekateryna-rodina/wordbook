8655d898acbf92638bf4e8696733f095e8219638
ekateryna-rodina <work.rodina@gmail.com>
2021-12-03 17:57:51 -0500

    redux test setup

--- client/src/components/CreateRecord/__test__/CreateRecord.test.tsx
+++ client/src/components/CreateRecord/__test__/CreateRecord.test.tsx
@@ -1,16 +1,28 @@
 import '@testing-library/jest-dom';
-import { render, screen } from '@testing-library/react';
-import { Provider } from 'react-redux';
 import { CreateRecord } from '..';
-import { store } from '../../../app/store';
+import { render, screen } from '../../../utils/test-utils';
 describe('Create record form', () => {
   it('Should render fields', () => {
-    render(
-      <Provider store={store}>
-        <CreateRecord show={true} />
-      </Provider>
-    );
+    render(<CreateRecord show={true} />);
     const wordInput = screen.getByTestId('wordInputTestId');
     expect(wordInput).toBeInTheDocument();
+
+    // const examplesInput = screen.getByTestId('examplesInputTestId');
+    // expect(examplesInput).toBeInTheDocument();
+
+    // const hintInput = screen.getByTestId('hintInputTestId');
+    // expect(hintInput).toBeInTheDocument();
+
+    // const setDropdown = screen.getByTestId('setDropdownnTestId');
+    // expect(setDropdown).toBeInTheDocument();
+
+    // const tagsAutocomplete = screen.getByTestId('tagsAutocompleteTestId');
+    // expect(tagsAutocomplete).toBeInTheDocument();
+
+    // const transcriptionInput = screen.getByTestId('transcriptionTestId');
+    // expect(transcriptionInput).toBeInTheDocument();
+
+    // const speechPartInput = screen.getByTestId('speechPartInputTestId');
+    // expect(speechPartInput).toBeInTheDocument();
   });
 });
