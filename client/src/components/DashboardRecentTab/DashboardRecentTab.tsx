d6ab239802cec7981e301a8be9e10d080de16cbb
ekateryna-rodina <work.rodina@gmail.com>
2022-01-11 18:40:05 -0500

    intersection observer hook

--- client/src/components/DashboardRecentTab/DashboardRecentTab.tsx
+++ client/src/components/DashboardRecentTab/DashboardRecentTab.tsx
@@ -1,11 +1,42 @@
 import React from 'react';
+import { useInView } from 'react-hook-inview';
 import styled from 'styled-components';
 import { useAppSelector } from '../../app/hooks';
 import { DashboardTabs } from '../../utils/enums';
 import Card from '../Card.style';
 import ScrollableHorizontalList from '../ScrollableHorizontalList.style';
 
-const SwipeHint = styled.div``;
+const recent = [
+  {
+    word: 'generous',
+    example: 'He was generous with both his time and his money',
+  },
+  {
+    word: 'sizzling',
+    example: 'It’s a sizzling hot day today!',
+  },
+  {
+    word: 'soothed',
+    example: 'At once, the rhythm and scent of the ocean soothed her',
+  },
+  {
+    word: 'generous',
+    example: 'He was generous with both his time and his money',
+  },
+  {
+    word: 'sizzling',
+    example: 'It’s a sizzling hot day today!',
+  },
+  {
+    word: 'soothed',
+    example: 'At once, the rhythm and scent of the ocean soothed her',
+  },
+  {
+    word: 'very long example',
+    example:
+      'Trained in a school where the principles of responsible government were still in an embryonic state, where the adroit management of coalitions and cabals was essential to the life of a political party, and where plots and counterplots were looked upon as a regular part of the political game, he acquired a dexterity and skill in managing men that finally gave him an almost autocratic power among his political followers.Trained in a school where the principles of responsible government were still in an embryonic state, where the adroit management of coalitions and cabals was essential to the life of a political party, and where plots and counterplots were looked upon as a regular part of the political game, he acquired a dexterity and skill in managing men that finally gave him an almost autocratic power among his political followers.',
+  },
+];
 
 const Word = styled.div`
   font-weight: 600;
@@ -37,63 +68,36 @@ const Content = styled.div`
   align-items: flex-start;
   height: 100%;
 `;
+const CardContainer = styled.div`
+  height: 80%;
+`;
 type RecentItemProps = {
   word: string;
   example: string;
-  inViewport: boolean;
 };
-const RecentItem = ({ word, example, inViewport }: RecentItemProps) => {
+const RecentItem = ({ word, example }: RecentItemProps) => {
+  const [ref, inView] = useInView({
+    rootMargin: '0% -50% 0% -50%',
+    threshold: 0,
+  });
   return (
-    <Card inViewport={inViewport}>
-      <Content>
-        <Example>{example}</Example>
-        <Word>{word}</Word>
-      </Content>
-    </Card>
+    <CardContainer ref={ref}>
+      <Card inViewport={inView}>
+        <Content>
+          <Example>{example}</Example>
+          <Word>{word}</Word>
+        </Content>
+      </Card>
+    </CardContainer>
   );
 };
 const DashboardRecentTab = () => {
   const activeTab = useAppSelector((state) => state.dashboard.tabSelected);
-  const recent = [
-    {
-      word: 'generous',
-      example: 'He was generous with both his time and his money',
-    },
-    {
-      word: 'sizzling',
-      example: 'It’s a sizzling hot day today!',
-    },
-    {
-      word: 'soothed',
-      example: 'At once, the rhythm and scent of the ocean soothed her',
-    },
-    {
-      word: 'generous',
-      example: 'He was generous with both his time and his money',
-    },
-    {
-      word: 'sizzling',
-      example: 'It’s a sizzling hot day today!',
-    },
-    {
-      word: 'soothed',
-      example: 'At once, the rhythm and scent of the ocean soothed her',
-    },
-    {
-      word: 'very long example',
-      example:
-        'Trained in a school where the principles of responsible government were still in an embryonic state, where the adroit management of coalitions and cabals was essential to the life of a political party, and where plots and counterplots were looked upon as a regular part of the political game, he acquired a dexterity and skill in managing men that finally gave him an almost autocratic power among his political followers.Trained in a school where the principles of responsible government were still in an embryonic state, where the adroit management of coalitions and cabals was essential to the life of a political party, and where plots and counterplots were looked upon as a regular part of the political game, he acquired a dexterity and skill in managing men that finally gave him an almost autocratic power among his political followers.',
-    },
-  ];
   return (
     <Container show={activeTab === DashboardTabs.Recent}>
-      <SwipeHint></SwipeHint>
       <ScrollableHorizontalList height={'30%'}>
         {recent.map((item, idx) => (
-          <RecentItem
-            {...{ ...item, inViewport: idx === 1 }}
-            key={item.word + Math.random()}
-          />
+          <RecentItem {...item} key={item.word + Math.random()} />
         ))}
       </ScrollableHorizontalList>
     </Container>
